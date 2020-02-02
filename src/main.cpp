2f9cecdca7e33a51246725320d2e25c942643587
Andrew Jackson <andrew.jackson@nztradegroup.co.nz>
2020-02-03 08:22:52 +1300

    adding ota update support

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <EspMQTTClient.h>
 #include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 
 const char *ssid = "HAPPYNET";
 const char *password = "***REMOVED***";
@@ -57,6 +58,50 @@ void setup(void)
   Serial.println(WiFi.localIP());
 
   client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+
+  // setup OTA Updating
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+    {
+      type = "sketch";
+    }
+    else
+    { // U_SPIFFS
+      type = "filesystem";
+    }
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+    {
+      Serial.println("Auth Failed");
+    }
+    else if (error == OTA_BEGIN_ERROR)
+    {
+      Serial.println("Begin Failed");
+    }
+    else if (error == OTA_CONNECT_ERROR)
+    {
+      Serial.println("Connect Failed");
+    }
+    else if (error == OTA_RECEIVE_ERROR)
+    {
+      Serial.println("Receive Failed");
+    }
+    else if (error == OTA_END_ERROR)
+    {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
 void getState(const String &payload)
@@ -116,4 +161,5 @@ void onConnectionEstablished()
 void loop(void)
 {
   client.loop();
+  ArduinoOTA.handle();
 }
